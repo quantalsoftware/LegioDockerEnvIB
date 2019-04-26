3a1d6256fb1047e4f413bc1fcdef151be5dd24c8
Quantal Software <projects@quantalsoftware.com.au>
2019-04-26 11:32:49 +1000

    Adjust folder

--- UpdateIBControllerFiles.sh
+++ UpdateIBControllerFiles.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-cp -a ~/LegioContainerFiles-master/IBControllerGatewayStart_$1.sh ~/ibcontroller.paper/IBControllerGatewayStart.sh
-cp -a ~/LegioContainerFiles-master/IBController_$1.ini ~/ibcontroller.paper/IBController.ini
-cp -a ~/LegioContainerFiles-master/jts_$1.ini ~/Jts/jts.ini
+cp -a ~/LegioDockerEnvIB-master/IBControllerGatewayStart_$1.sh ~/ibcontroller.paper/IBControllerGatewayStart.sh
+cp -a ~/LegioDockerEnvIB-master/IBController_$1.ini ~/ibcontroller.paper/IBController.ini
+cp -a ~/LegioDockerEnvIB-master/jts_$1.ini ~/Jts/jts.ini
